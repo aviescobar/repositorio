d2b9473775d6ee2318f8cd170cbbeecb43a790a5
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-29 09:43:35 -0600

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,6 @@ import math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+
+Primera parte: Solicitar el número de vectores con el que se va a trabajar.
+print("\033[1;34m"+"\n¡Bienvenido al programa de vectores de XFORCE!"+"\033[0;m"+"\n\n¿Con cuántos vectores (n) deseas trabajar?")
