4961e4cc5fa33dc49dc9484c4d752053057eaca0
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-29 09:45:12 -0600

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -8,8 +8,9 @@ import numpy as np
 print("\033[1;34m"+"\n¡Bienvenido al programa de vectores de XFORCE!"+"\033[0;m"+"\n\n¿Con cuántos vectores (n) deseas trabajar?")
 numvectors=input("\033[1;32m"+"n= "+"\033[0;m")
 while not(numvectors.isdigit()) or not(int(numvectors)>0):
-  print("\033[1;31m"+"**"+"\033[4;30m"+ "Lo sentimos, lo que has ingresado no es un número valido, vuelve a intentarlo"+"\033[0;m")
+    print("\033[1;31m"+"**"+"\033[4;30m"+ "Lo sentimos, lo que has ingresado no es un número valido, vuelve a intentarlo"+"\033[0;m")
     numvectors= input("\033[1;32m"+"n= "+"\033[0;m")
 
 #Segunda parte: Definir la información que me dará de cada vector y almacenarlo.
 vectoresinfo=[]
+opValInfo=["a","b","c","1","2"]
