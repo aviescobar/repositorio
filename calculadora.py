06858a73a8d7d542bc2ec074ef2630d26f51f5f0
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-29 10:05:50 -0600

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -57,3 +57,4 @@ for i in range (int(numvectors)):
                 if alpha.count(".")==1:
                     if (not(alpha.split(".")[0].isdigit())) or (not(alpha.split(".")[1].isdigit())):
                         print("\033[1;31m"+"**"+"\033[4;30m"+"Los elementos antes y después del punto deben ser números."+"\033[0;m")
+                        validAlpha = 0
